b1d090e0efda0e61f4a0a9673e9a605d028969b2
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-03-31 18:38:41 -0300

    v1.0.0

--- libs/git_rules.sh
+++ libs/git_rules.sh
@@ -3,12 +3,12 @@
 ####################
 
 # Don't run if caller_folder is not a git repository
-if [ -z "$(git status)" ]; then
+if [[ -z "$(git status)" ]]; then
   echo "You're are not in a project folder. \e[0m run \e[32mdt new\e[0m"
   exit 1
 fi
 # Don't run if branch is main
-if [ "$(git rev-parse --abbrev-ref HEAD)" = "main" && $1 != "new" &&  $1 != "edit" &&  $1 != "patch" ]; then
+if [[ "$(git rev-parse --abbrev-ref HEAD)" = "main" ] && [ $1 != "new" ] && [ $1 != "edit" ] && [ $1 != "patch" ]]; then
   echo "You are on main branch, aborting..."
   echo "Please checkout to a version branch and run this script again"
   echo "To create a new patch run: dt patch"
