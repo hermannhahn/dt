2c4e71d917f44637940ec4c0bb68c0b1043297d7
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-03-31 16:00:22 -0300

    v1.0.0

--- libs/update_check.sh
+++ libs/update_check.sh
@@ -1,11 +1,12 @@
 function update_check {
   # Check for updates
-  MAIN_VERSION=1
+  MAIN_VERSION=$(curl -s https://raw.githubusercontent.com/hermannhahn/dt/main/package.json | grep -oP '"version": "\K(.*)(?=")')
   # Get local version from package.json
   LOCAL_VERSION=$(cat $DT_DIR/package.json | grep -oP '"version": "\K(.*)(?=")')
   # Compare versions
   if [ "$RELEASE_VERSION" != "$LOCAL_VERSION" ]; then
   # Add green tip
     echo -e "\e[32mUpdate available!\e[0m Run \e[32mdt update\e[0m to update."
+  fi
 fi
 update_check
