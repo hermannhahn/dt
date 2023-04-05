af4d6a1794d6d94f727ab170cb6722876c600619
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-04-05 16:21:43 -0300

    "v0.0.0"

--- src/utils/terminal-log.ts
+++ src/utils/terminal-log.ts
@@ -3,27 +3,21 @@ import { Icons } from "utils/icon-text"
 export class terminal {
 	public static log(icon: string, text?: string) {
 		const msgIcon = new Icons(icon)
-		const msg = text !== undefined ? text : ""
-		console.log("  " + msgIcon.print() + " " + msg)
+		const msg = text !== undefined ? " " + text : ""
+		console.log(" " + msgIcon.print() + msg)
 	}
+
 	public static logInline(icon: string, text?: string) {
 		const msgIcon = new Icons(icon)
 		const msg = text ? " " + text : ""
 		process.stdout.write("  " + msgIcon.print() + msg)
 	}
-	public static debug(text: string) {
-		this.log("debug", text)
-	}
-	public static error(text?: string) {
-		this.log("error", text !== undefined ? text : "")
-	}
+
 	public static success(text?: string) {
 		this.log("success", text)
 	}
-	public static warn(text?: string) {
-		this.log("warn", text)
-	}
-	public static info(text?: string) {
-		this.log("info", text)
+
+	public static error(text?: string) {
+		this.log("error", text)
 	}
 }
