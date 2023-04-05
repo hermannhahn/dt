23f687accd579b1b1336ede4758c18cd45219400
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-04-05 20:24:30 -0300

    "v0.0.0"

--- src/libs/git.ts
+++ src/libs/git.ts
@@ -5,7 +5,7 @@ export class git {
 		try {
 			const status = new CommandRunner(`git status`)
 			const result = await status.run()
-			console.log(result.includes("nothing to commit"))
+			console.log(result.stdout.includes("nothing to commit"))
 			if (result.includes("nothing to commit")) {
 				return false
 			}
