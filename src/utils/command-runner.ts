c4a123f8dd58991fa52e88ba8b5b5bc25b531aa2
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-04-05 20:50:24 -0300

    "v0.0.0"

--- src/utils/command-runner.ts
+++ src/utils/command-runner.ts
@@ -13,7 +13,7 @@ export class CommandRunner {
 		try {
 			const [cmd, ...args] = this.command.split(" ")
 			const childProcess = spawn(cmd, args, { stdio: this.promise })
-			return childProcess.stdout
+			return childProcess.stdio
 		} catch (error: any) {
 			return error
 		}
