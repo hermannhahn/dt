11b5870c49a25f667f3edab16db5bb75de26012c
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-04-07 10:33:54 -0300

    v0.0.0

--- src/modules/cli/save.ts
+++ src/modules/cli/save.ts
@@ -38,8 +38,7 @@ export const Save = async () => {
 
 				terminal.log("success", "Successfully saved project")
 			} catch (error: any) {
-				// white red color: \x1b[37m\x1b[31m
-				terminal.error("[\x1b[31merror\x1b[0m] " + error.result)
+				terminal.error("[\x1b[31mfail\x1b[0m]")
 			}
 		})
 }
