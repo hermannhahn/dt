346988f16c3181ba473ed26beaeee071c751b0cc
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-04-07 01:42:42 -0300

    v0.0.0

--- src/modules/cli/save.ts
+++ src/modules/cli/save.ts
@@ -19,8 +19,8 @@ export const Save = async () => {
 				const message = opts.message || `v${version}`
 				const status: any = await git.branch.status
 				const commands = [
-					terminal.debug("Searching for Bugs..."),
 					await git.add("."),
+					terminal.debug("Searching for Bugs..."),
 					await git.commit(message),
 					await git.push(),
 					await git.push("--tags"),
