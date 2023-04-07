a3b1055b3d39e2b587eee9dad7eff1fa3fd10bff
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-04-07 17:55:28 -0300

    v0.0.0

--- src/modules/cli/save.ts
+++ src/modules/cli/save.ts
@@ -20,7 +20,7 @@ export const Save = async () => {
 				const status: any = git.branch.status()
 				const add = async () => {
 					const AddFiles: any = await git.add(".")
-					terminal.debug(AddFiles.result)
+					terminal.debug(AddFiles.error)
 					if (AddFiles.error) {
 						terminal.log("[\x1b[31mfail\x1b[0m]")
 						throw new Error(`${AddFiles.error}`)
