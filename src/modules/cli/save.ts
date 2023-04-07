dcacc88d1fb8c8c25ded36046825d86855fa9575
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-04-07 02:27:35 -0300

    v0.0.0

--- src/modules/cli/save.ts
+++ src/modules/cli/save.ts
@@ -15,21 +15,28 @@ export const Save = async () => {
 			try {
 				const version = packageJson.get("version")
 				const name = packageJson.get("name")
-				terminal.logInline("save", `Saving ${name} v${version} `)
+				terminal.log("save", `Saving ${name} v${version} `)
 				const message = opts.message || `v${version}`
 				const status: any = await git.branch.status
 				const commands = [
+					terminal.logInline("save", "Adding files... "),
 					await git.add("."),
+					console.log("[\x1b[32msuccess\x1b[0m]"),
+					terminal.logInline("save", "Commiting files... "),
 					await git.commit(message),
+					console.log("[\x1b[32msuccess\x1b[0m]"),
+					terminal.logInline("save", "Pushing files... "),
 					await git.push(),
+					console.log("[\x1b[32msuccess\x1b[0m]"),
+					terminal.logInline("save", "Pushing tags... "),
 					await git.push("--tags"),
+					console.log("[\x1b[32msuccess\x1b[0m]"),
 				]
 				if (status) {
 					await Promise.all(commands)
 				}
 
-				console.log("[\x1b[32msuccess\x1b[0m]")
-				terminal.log("success", "Project saved")
+				terminal.log("success", "Successfully saved project")
 			} catch (error: any) {
 				terminal.error("Error: " + error.data)
 			}
