310a540c33dcadc37903c8558706b59ce0d71926
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-05-04 12:27:43 -0300

    0.0.1

--- src/modules/cli.ts
+++ src/modules/cli.ts
@@ -1,3 +1,4 @@
+import { Deploy } from "modules/cli/deploy"
 import { Init } from "modules/cli/init"
 import { New } from "modules/cli/new"
 import { Save } from "modules/cli/save"
@@ -8,4 +9,5 @@ export class Cli {
 	static new = New
 	static init = Init
 	static template = Template
+	static deploy = Deploy
 }
