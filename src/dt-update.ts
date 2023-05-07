e256a5ddb513167586738af28c60e00dde9d170d
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-05-07 19:31:26 -0300

    0.0.4

--- src/dt-update.ts
+++ src/dt-update.ts
@@ -29,6 +29,13 @@ const getLatestVersion = async () => {
 const updateBinary = async () => {
 	const sys = os.platform()
 	if (sys === 'win32') {
+		// Get dt folder path
+		const dtPath: any = new Command('where', ['dt.exe'])
+		const dtPathString = dtPath.split('\n')[0]
+		const dtFolderPath = dtPathString.replace('dt.exe', '')
+		// Go to dt folder
+		process.chdir(dtFolderPath)
+		// Download binary
 		const download = await fetch(
 			'https://github.com/hermannhahn/dt/blob/76d373c54f237a7633982c9cea66fb8360b3ebf9/dist/dt-win.exe'
 		)
@@ -36,6 +43,13 @@ const updateBinary = async () => {
 		const downloadBuffer = Buffer.from(downloadData)
 		fs.writeFileSync('dt.exe', downloadBuffer)
 	} else if (sys === 'linux') {
+		// Get dt folder path
+		const dtPath: any = new Command('which', ['dt'])
+		const dtPathString = dtPath.split('\n')[0]
+		const dtFolderPath = dtPathString.replace('dt', '')
+		// Go to dt folder
+		process.chdir(dtFolderPath)
+		// Download binary
 		const download = await fetch(
 			'https://github.com/hermannhahn/dt/blob/76d373c54f237a7633982c9cea66fb8360b3ebf9/dist/dt-linux'
 		)
@@ -43,6 +57,13 @@ const updateBinary = async () => {
 		const downloadBuffer = Buffer.from(downloadData)
 		fs.writeFileSync('dt', downloadBuffer)
 	} else if (sys === 'darwin') {
+		// Get dt folder path
+		const dtPath: any = new Command('which', ['dt'])
+		const dtPathString = dtPath.split('\n')[0]
+		const dtFolderPath = dtPathString.replace('dt', '')
+		// Go to dt folder
+		process.chdir(dtFolderPath)
+		// Download binary
 		const download = await fetch(
 			'https://github.com/hermannhahn/dt/blob/76d373c54f237a7633982c9cea66fb8360b3ebf9/dist/dt-macos'
 		)
