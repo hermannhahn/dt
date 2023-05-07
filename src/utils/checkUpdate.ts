155a0ab511f67cde9843a1ebaf3e01af8e1ac632
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-05-07 18:52:59 -0300

    0.0.4

--- src/utils/checkUpdate.ts
+++ src/utils/checkUpdate.ts
@@ -1,6 +1,7 @@
 import { Command } from 'utils/command-runner'
 import { terminal } from 'utils/terminal-log'
 
+// Check for updates
 export const checkUpdate = async () => {
 	const result: any = new Command('dt.exe', ['--version'])
 	if (result.code === 0) {
@@ -13,6 +14,7 @@ export const checkUpdate = async () => {
 	}
 }
 
+// Get latest version
 const getLatestVersion = async () => {
 	const packageJson = await fetch(
 		'https://github.com/hermannhahn/dt/blob/76d373c54f237a7633982c9cea66fb8360b3ebf9/package.json'
