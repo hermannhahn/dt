c7636ffb9e29ba08d02cefe10c2111a08153b8a3
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-05-08 22:13:01 -0300

    0.0.9

--- create-release.sh
+++ create-release.sh
@@ -7,7 +7,6 @@ GITHUB_REPO=$(node -p -e "require('./package.json').repository.url.split('/')[4]
 
 # Create a new release on GitHub
 gh release create \
-  --user $GITHUB_USER \
   --repo $GITHUB_REPO \
   --tag $VERSION \
   --name "Release $VERSION" \
